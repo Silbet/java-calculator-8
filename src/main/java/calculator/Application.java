8e43bcc439cee91a265ec87d6be23ee86a593052
Junbeom <warbringer98@gmail.com>
2025-10-20 06:39:38 +0900

    Feat: 문자열에서 숫자 추출하기 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,24 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+    private static String delimiters = ",|:";
+
+    public static List<Integer> extractNumber(String s) {
+        List<Integer> numbers = new ArrayList<>();
+
+        String[] parts = s.split(delimiters);
+
+        // 추출한 숫자를 List<Integer> 형태로 변환 후 리턴
+        for (String part : parts) {
+            numbers.add(Integer.parseInt(part));
+        }
+
+        return numbers;
+    }
 
     public static int addNumber(List<Integer> numbers) {
         int sum = 0;
