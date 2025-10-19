01c79e63ca7e555cac5427cc8ed4ec3c6b2de2af
Junbeom <warbringer98@gmail.com>
2025-10-20 05:45:26 +0900

    Feat: 숫자 더하기 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,17 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
+
+    public static int addNumber(List<Integer> numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
